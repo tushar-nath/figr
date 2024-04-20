0219b1249355e56140b960211eec0bc7fa9c2f11
tushar-nath <tusharnath10@gmail.com>
2024-04-20 22:11:16 +0530

    FEAT: Updated the spacing component

--- src/components/items/spacing.tsx
+++ src/components/items/spacing.tsx
@@ -1,15 +1,36 @@
-"use client";
-
-import React from "react";
+import React, { useState } from "react";
 import { Select } from "antd";
 import { VariantCount } from "./variantCount";
 import { useSpaceContext } from "@/lib/context/spaceContext";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Input } from "../ui/input";
+import { Trash2 } from "lucide-react";
 
 const { Option } = Select;
+
+const sizes = [
+  { sizeName: "xxxs", sizePx: "2px", sizeRem: "0.125rem", isDeleted: false },
+  { sizeName: "xxs", sizePx: "4px", sizeRem: "0.25rem", isDeleted: false },
+  { sizeName: "xs", sizePx: "6px", sizeRem: "0.375rem", isDeleted: false },
+  { sizeName: "sm", sizePx: "8px", sizeRem: "0.5rem", isDeleted: false },
+  { sizeName: "md", sizePx: "12px", sizeRem: "0.75rem", isDeleted: false },
+  { sizeName: "lg", sizePx: "16px", sizeRem: "1rem", isDeleted: false },
+  { sizeName: "xl", sizePx: "20px", sizeRem: "1.25rem", isDeleted: false },
+  { sizeName: "2xl", sizePx: "24px", sizeRem: "1.5rem", isDeleted: false },
+  { sizeName: "3xl", sizePx: "32px", sizeRem: "2rem", isDeleted: false },
+];
 
 export const SpacingComponent = () => {
   const { spacing, setSpacing, variantCount, setVariantCount } =
     useSpaceContext();
+  const [updatedSizes, setUpdatedSizes] = useState(sizes);
 
   const handleSpacingChange = (value: any) => {
     setSpacing(value);
@@ -17,6 +38,25 @@ export const SpacingComponent = () => {
 
   const handleVariantCountChange = (value: number) => {
     setVariantCount(value);
+  };
+
+  const handleSizeChange = (index: number, field: string, value: string) => {
+    const newSizes = [...updatedSizes];
+    const size = newSizes[index];
+
+    if (field === "sizePx") {
+      const pixelValue = value.endsWith("px") ? parseInt(value, 10) : 0;
+      const remValue = pixelValue / 16; // Assuming 1rem = 16px
+      newSizes[index] = {
+        ...size,
+        sizePx: value,
+        sizeRem: remValue.toFixed(3) + "rem",
+      };
+    } else {
+      newSizes[index] = { ...size, [field]: value };
+    }
+
+    setUpdatedSizes(newSizes);
   };
 
   return (
@@ -45,7 +85,48 @@ export const SpacingComponent = () => {
           onChange={handleVariantCountChange}
         />
       </div>
-      <div className="h-[800px] w-2/3 border rounded-[25px]"></div>
+      <div className="h-[800px] w-2/3 border rounded-[25px]">
+        <div className="py-10 px-6">
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-1/3">Variable Name</TableHead>
+                <TableHead className="w-1/3">Size (px)</TableHead>
+                <TableHead className="w-1/3">Size (rem)</TableHead>
+                <TableHead className="w-1/3 text-right"></TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {updatedSizes.map((size, index) => (
+                <TableRow key={size.sizeName}>
+                  <TableCell className="font-medium">
+                    <Input
+                      defaultValue={size.sizeName}
+                      onChange={(e) =>
+                        handleSizeChange(index, "sizeName", e.target.value)
+                      }
+                    />
+                  </TableCell>
+                  <TableCell>
+                    <Input
+                      defaultValue={size.sizePx}
+                      onChange={(e) =>
+                        handleSizeChange(index, "sizePx", e.target.value)
+                      }
+                    />
+                  </TableCell>
+                  <TableCell>
+                    <Input disabled value={size.sizeRem} />
+                  </TableCell>
+                  <TableCell className="text-right">
+                    <Trash2 />
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
+      </div>
     </div>
   );
 };
